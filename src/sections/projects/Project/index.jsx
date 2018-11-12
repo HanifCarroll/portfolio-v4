3fbc4753d3101495cd02405b5d9f4bc725329731
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-12 09:17:26 -0500

    Modify CSS to get project overlay working.

--- src/sections/projects/Project/index.jsx
+++ src/sections/projects/Project/index.jsx
@@ -4,8 +4,23 @@ import styles from "./styles.module.scss";
 export class Project extends React.Component {
   render() {
     return (
-      <div className={styles.container}>
-        <img src={this.props.image} alt={this.props.title} />
+      <div
+        className={styles.container}
+        style={{
+          background: `url(${this.props.image}) center center/cover`,
+        }}
+      >
+        <div className={styles.overlay}>
+          <p>Test</p>
+          <p>Test</p>
+          <p>Test</p>
+          <p>Test</p>
+        </div>
+        <img
+          className={styles.image}
+          src={this.props.image}
+          alt={this.props.title}
+        />
       </div>
     );
   }
