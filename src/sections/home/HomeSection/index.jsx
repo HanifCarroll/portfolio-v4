b15429e9e37b1fe3b17ab7368a54b413bd741ffe
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-14 09:40:39 -0500

    Modify Home section.

--- src/sections/home/HomeSection/index.jsx
+++ src/sections/home/HomeSection/index.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import { Jumbotron } from "reactstrap";
+
 export class HomeSection extends React.Component {
   render() {
     return (
-      <Jumbotron id="home" className={styles.container}>
-        <div className={styles["image-container"]}>
-          <img
-            className="img-fluid"
-            src={require("../profile.jpg")}
-            alt="Hanif Carroll"
-          />
+      <div className={styles.container}>
+        <img src={require("../profile.jpg")} alt="Hanif Carroll" />
+        <div className={styles.textContainer}>
+          <h1 className={styles.heading}>Hanif Carroll</h1>
+          <h3 className={styles.subheading}>Web/Mobile Developer</h3>
         </div>
-        <div className={styles["text-container"]}>
-          <h1 className={styles.heading}>Hanif </h1>
-          <h1 className={styles.heading}>Carroll</h1>
-        </div>
-      </Jumbotron>
+      </div>
     );
   }
 }
